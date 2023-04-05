24c187cbe8adf063e4f46f74bd0216649a2ee1e9
Lodo2115 <113630939+Lodo2115@users.noreply.github.com>
2023-04-05 13:42:57 +0200

    esa

--- main.js
+++ main.js
@@ -5,18 +5,18 @@ $(document).ready(function () {
   $('#B_Haslo').click(function () {
     haslo = $('#I_Haslo').val();
     $('#Div_Haslo').hide();
-
+    $('#Div_Okno_Gry').css('display','inline-block');
     var dlg_haslo = haslo.length;
     var litery = "";
     console.log(dlg_haslo);
     for (let i = 0; i < dlg_haslo; i++) {
       litery += "_";
     }
-    $('body').append("<p id='litery_haslo'>" + litery + "</p>");
+    $('#Div_Okno_Gry').append("<p id='litery_haslo'>" + litery + "</p>");
 
     for (var i = 0; i < przyicski.length; i++) {
-      if (i % 5 == 0 && i != 0) $('body').append('<br>');
-      $('body').append('<button class="litera" id="' + przyicski[i] + '" value="'+przyicski[i]+'">' + przyicski[i] + '</button>');
+      if (i % 5 == 0 && i != 0) $('#Div_Okno_Gry').append('<br>');
+      $('#Div_Okno_Gry').append('<button class="litera" id="' + przyicski[i] + '" value="'+przyicski[i]+'">' + przyicski[i] + '</button>');
     }
 
     
@@ -26,3 +26,4 @@ $(document).ready(function () {
     });
   });
 });
+
